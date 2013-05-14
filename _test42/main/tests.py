b2b99d473783acdab9800547131579db02f59410
Yaroslav Avdeev <yaroslav.avdeev@gmail.com>
2013-05-14 17:47:48 +0300

    ticket:1 not working test

--- _test42/main/tests.py
+++ _test42/main/tests.py
@@ -7,11 +7,35 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from models import Profile, Contacts
+
 
 class Test(TestCase):
     
     fixtures = ['profile.json', 'contacts.json']
+
+    def test_profile_exists(self):
+        profiles = Profile.objects.all()
+        profiles = list(profiles)
+        self.failUnless(profiles)
+        
+        p = profiles[0]
+        self.failUnlessEqual(p.name, 'Some name')
+        
+        contacts = p.contacts_set.all()
+        contacts = list(contacts)
+        self.failUnless(contacts)
+        
+        c = contacts[0]
+        self.failUnlessEqual(c.jabber, 'some@email.com')
     
     def test_profile_page(self):
         res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
+        
+        self.failUnless(res.context['profile'])
+        
+        
+        
+
+        
