b985e533db768067364f57324bb6eb2e7d502266
Yaroslav Avdeev <yaroslav.avdeev@gmail.com>
2013-05-16 23:06:51 +0300

    ticket: settings fix

--- _test42/settings.py
+++ _test42/settings.py
@@ -111,6 +111,12 @@ TEMPLATE_DIRS = (
 
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
     '_test42.main.context_processors.settings_processor',
 )
 
