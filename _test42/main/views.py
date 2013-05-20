a5da9e85199113c475c002bf7421cf0865270449
Yaroslav Avdeev <yaroslav.avdeev@gmail.com>
2013-05-20 22:06:30 +0300

    ticket:6 ajax form

--- _test42/main/views.py
+++ _test42/main/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
+
 from models import Profile, RequestLog
 from forms import ProfileEditForm
 
@@ -18,14 +19,23 @@ def profile_edit(request, profile_id=None):
     if not profile_id or not profile_id.isdigit():
         raise Http404
     
+    _is_ajax = request.is_ajax()
+    
     profile = get_object_or_404(Profile, id=int(profile_id))
     if request.POST:
         form = ProfileEditForm(data=request.POST, files=request.FILES, instance=profile)
-        if form.is_valid():
+        
+        _is_valid = form.is_valid()
+        if _is_valid:
             form.save()
-            
+        
+        if not _is_ajax and _is_valid:
             return HttpResponseRedirect(request.META.get('HTTP_REFERER', \
-                                        reverse('profile_edit', kwargs={'profile_id': profile.id })))
+                                            reverse('profile_edit', kwargs={'profile_id': profile.id })))
+
+        if _is_ajax:
+            return render(request, 'profile_edit_errors.html', {'form': form})
+
     else:
         form = ProfileEditForm(instance=profile)
         
