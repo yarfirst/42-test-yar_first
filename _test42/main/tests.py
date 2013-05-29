1a84cbff01d0b1a7d91da5c24b3e2156daa86993
Yaroslav Avdeev <yaroslav.avdeev@gmail.com>
2013-05-29 12:06:43 +0300

    ticket:15 tests update & fixes

--- _test42/main/tests.py
+++ _test42/main/tests.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,8 +7,11 @@ Replace this with more appropriate tests for your application.
 """
 import sys
 from StringIO import StringIO
+from urlparse import urlparse
 
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.core.management import call_command
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from models import Profile, RequestLog, ModelChangesLog
@@ -18,66 +22,70 @@ class Test(TestCase):
     def test_profile_exists(self):
         profiles = Profile.objects.all()
         profiles = list(profiles)
-        self.failUnless(profiles)
+        self.assertTrue(profiles)
 
         p = profiles[0]
-        self.failUnlessEqual(p.name, 'Some name')
+        self.assertEqual(p.name, 'Some name')
 
     def test_profile_page(self):
         res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
 
-        self.failUnless(res.context['profile'])
+        self.assertTrue(res.context['profile'])
 
         p = Profile.objects.all()[0]
-        self.failUnless(res.content.find(p.name))
-        self.failUnless(res.content.find('edit_in_admin'))
+        self.assertContains(res, p.name)
+        self.assertContains(res, 'edit_in_admin')
 
     def test_request_middleware(self):
         res = self.client.get('/requests/')
         self.assertEqual(res.status_code, 200)
 
         request_logs = RequestLog.objects.all().order_by('id')
-        self.failUnless(request_logs)
+        self.assertTrue(request_logs)
 
-        self.failUnless(res.context['request_logs'])
+        self.assertTrue(res.context['request_logs'])
 
         context_request_logs = res.context['request_logs']
 
         log_length = len(context_request_logs)
-        self.failUnless(log_length)
-        self.failIf(log_length > 10)
+        self.assertTrue(log_length)
+        self.assertFalse(log_length > 10)
 
-        self.failUnless(res.content.find('/requests/'))
+        self.assertTrue(res.content.find('/requests/'))
 
     def test_context_processor(self):
         res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
 
-        self.failUnless(res.context['SETTINGS'])
-        context_settings = res.context['SETTINGS']
-
-        from django.conf import settings
-        self.assertEqual(context_settings.DEBUG, settings.DEBUG)
+        self.assertTrue(res.context['SETTINGS'])
 
     def test_edit_page(self):
         profile = Profile.objects.all()[0]
 
-        profile_edit_url = '/edit/%s/' % profile.id
+        profile_edit_url = reverse('profile_edit', \
+                                   kwargs={'profile_id': profile.id})
 
         res = self.client.get(profile_edit_url)
-        self.assertEqual(res.status_code, 302)
+        # self.assertEqual(res.status_code, 302)
+        # на мой взгляд предыдущщий вариант прощще
+        next_url = REDIRECT_FIELD_NAME + '=' + urlparse(profile_edit_url).path
+        dest_url = reverse('login_page')
+        dest_url = dest_url + '?' + next_url
+
+        self.assertRedirects(res, dest_url)
 
         self.client.login(username='admin', password='admin')
         res = self.client.get(profile_edit_url)
-        self.assertEqual(res.status_code, 200)
 
-        self.failUnless(res.content.find('id_name'))
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, 'id_name')
 
         data = profile.__dict__
         data['name'] = 'TEST NAME'
         res = self.client.post(profile_edit_url, data)
-        self.assertEqual(res.status_code, 302)
+        # self.assertEqual(res.status_code, 302)
+        self.assertRedirects(res, profile_edit_url)
 
         profile = Profile.objects.all()[0]
         self.assertEqual(profile.name, data['name'])
@@ -100,11 +108,11 @@ class Test(TestCase):
         _out_text = _stdout.read()
         _err_text = _stderr.read()
 
-        self.failUnless(len(_out_text))
-        self.failUnless(len(_err_text))
+        self.assertTrue(len(_out_text))
+        self.assertTrue(len(_err_text))
 
-        self.failUnless('_test42.main.models.Profile' in _out_text)
-        self.failUnless('_test42.main.models.RequestLog' in _err_text)
+        self.assertTrue('_test42.main.models.Profile' in _out_text)
+        self.assertTrue('_test42.main.models.RequestLog' in _err_text)
 
     def test_signals(self):
         rlog = RequestLog.objects.create(method='GET', \
@@ -113,16 +121,16 @@ class Test(TestCase):
 
         changes_log = ModelChangesLog.objects.filter(name='RequestLog', \
                                  action='create')
-        self.failUnless(changes_log)
+        self.assertTrue(changes_log)
 
         rlog.remote_addr = 'http://localhost:8080/'
         rlog.save()
 
         changes_log = ModelChangesLog.objects.filter(name='RequestLog', \
                                  action='edit')
-        self.failUnless(changes_log)
+        self.assertTrue(changes_log)
 
         rlog.delete()
         changes_log = ModelChangesLog.objects.filter(name='RequestLog', \
                                  action='delete')
-        self.failUnless(changes_log)
+        self.assertTrue(changes_log)
